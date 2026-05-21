3cbfca95a406ab4233fafa27888063d1dbc559fe
MarkAvrelij <m.chuguy@gmail.com>
2026-05-21 14:37:15 +0200

    fix build

--- src/app/core/http-error.interceptor.ts
+++ src/app/core/http-error.interceptor.ts
@@ -1,17 +1,17 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
-import { ErrorService } from './error.service';
+//import { ErrorService } from './error.service';
 
 export const httpErrorInterceptor: HttpInterceptorFn = (request, next) => {
-  const errorService = inject(ErrorService);
+  ///const errorService = inject(ErrorService);
 
   return next(request).pipe(
     catchError((error: unknown) => {
       if (error instanceof HttpErrorResponse) {
         const message =
           error.error?.message ?? error.message ?? `Request failed (${error.status})`;
-        errorService.show(message);
+       // errorService.show(message);
       }
 
       return throwError(() => error);
